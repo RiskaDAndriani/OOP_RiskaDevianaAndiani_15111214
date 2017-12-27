0ed8bb437f0693d1aefd1f750595881344976a8d
RiskaDAndriani <32569810+RiskaDAndriani@users.noreply.github.com>
2017-12-27 13:00:18 +0700

    Pertemuan 11

---
+++ Pertemuan 11 (Encapsulation)/B.java
@@ -0,0 +1,11 @@
+public class B{
+    A a = new A();
+
+    public void setAaInA(){
+        a.setAa(aA);
+    }
+
+    public String getAaInA(){
+        return a.getAa();
+    }
+}
---
+++ Pertemuan 11 (Encapsulation)/Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[]args){
+        A a = new A();
+
+        a.setAa("Ini Aa");
+        System.out.println(a.getAa());
+    }
+}
